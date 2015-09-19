c34bd82914c2a26d8118e1742dd0c20a0628ce72
Nils Jacob Berland <njberland@gmail.com>
2015-09-19 09:54:11 +0200

    Update and rename BasicSensors.ino to DustLLAP.ino

--- DustLLAP.ino
+++ DustLLAP.ino
@@ -1,9 +1,4 @@
 /* code for evalution made for the Frisk Luft project at www.friskby.no
-
-Portions are based on code from Ciseco.co.uk and http://www.howmuchsnow.com/arduino/airquality/ 
-
-The code assume a Sharp dust sensor and a DHT temperature and humidity sensor
-
 */
 
 #include <LLAPSerial.h>
@@ -20,14 +15,13 @@ float voMeasured = 0;
 float calcVoltage = 0;
 float dustDensity = 0;
 
-#define DEVICEID "DH"	// this is the LLAP device ID
+#define DEVICEID "DS"	// this is the LLAP device ID
 #define DHTPIN 4     // what I/O the DHT-22 data pin is connected to
 #define DHTTYPE DHT11   // DHT 22  (AM2302)
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-
   pinMode(ledPower, OUTPUT);
   Serial.begin(115200);
   pinMode(8, OUTPUT);		// switch on the radio
@@ -42,54 +36,23 @@ void setup() {
 
 void loop() {
 
-  //delay(1000);
-  // print the string when a newline arrives:
-
-  if (LLAP.bMsgReceived) {
-    Serial.print(F("msg:"));
-    Serial.println(LLAP.sMessage);
-    LLAP.bMsgReceived = false;	// if we do not clear the message flag then message processing will be blocked
-  }
-
   // every 10 seconds
   static unsigned long lastTime = millis();
-  if (millis() - lastTime >= 5000)
+  if (millis() - lastTime >= 10000)
   {
     lastTime = millis();
-
     digitalWrite(ledPower, LOW); // power on the LED
     delayMicroseconds(samplingTime);
-
     voMeasured = analogRead(measurePin); // read the dust value
-
     delayMicroseconds(deltaTime);
     digitalWrite(ledPower, HIGH); // turn the LED off
     delayMicroseconds(sleepTime);
 
     // 0 - 5V mapped to 0 - 1023 integer values
-    // recover voltage
-    calcVoltage = voMeasured * (5.0 / 1024.0);
-
-    // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
-    // Chris Nafis (c) 2012
-    dustDensity = 0.17 * calcVoltage - 0.1;
+    calcVoltage = voMeasured * (5.0 / 1023.0);
     int h = dht.readHumidity() * 10;
     int t = dht.readTemperature() * 10;
     int d = voMeasured; 
-    Serial.println();
-    /*     
-     Serial.print("Raw Signal Value (0-1023): ");
-     Serial.print(voMeasured);
-     Serial.print(" - Voltage: ");
-     Serial.print(calcVoltage);
-     Serial.print(" - Dust Density: ");
-     Serial.println(dustDensity); // unit: mg/m3
-     Serial.print(" Temp: ");
-     Serial.print(t);
-     Serial.print(" Humid: ");
-     Serial.println(h);
-     */
-
     // check if returns are valid, if they are NaN (not a number) then something went wrong!
     if (isnan(t) || isnan(h) || isnan(d)) {
       LLAP.sendMessage(F("ERROR"));
@@ -100,7 +63,11 @@ void loop() {
       LLAP.sendInt("DST", d);
     }
   }
+  
+  // test if we have received anything on LLAP
+  if (LLAP.bMsgReceived) {
+    Serial.print(F("msg:"));
+    Serial.println(LLAP.sMessage);
+    LLAP.bMsgReceived = false;	// if we do not clear the message flag then message processing will be blocked
+  }
 }
-
-
-
